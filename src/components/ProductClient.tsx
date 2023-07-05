03df6c1edfa370f5c3d0417f845bf562a5c955cd
min33sky <min33sky@naver.com>
2023-07-05 23:41:26 +0900

    feat: add remove product

--- src/components/ProductClient.tsx
+++ src/components/ProductClient.tsx
@@ -5,13 +5,18 @@ import {
   columns,
 } from '@/app/(dashboard)/[storeId]/products/columns';
 import { useParams, useRouter } from 'next/navigation';
-import React from 'react';
+import React, { startTransition } from 'react';
 import Heading from './Heading';
 import { Button } from './ui/button';
 import { Plus } from 'lucide-react';
 import { Separator } from './ui/separator';
 import { DataTable } from './ui/data-table';
 import ApiList from './ApiList';
+import { useModal } from '@/app/hooks/useModal';
+import { useMutation } from '@tanstack/react-query';
+import axios, { AxiosError } from 'axios';
+import toast from 'react-hot-toast';
+import AlertModal from './modals/AlertModal';
 
 interface ProductClientProps {
   data: ProductColumn[];
@@ -20,9 +25,39 @@ interface ProductClientProps {
 export default function ProductClient({ data }: ProductClientProps) {
   const params = useParams();
   const router = useRouter();
+  const { onClose, targetId } = useModal();
+
+  /**
+   * @description
+   * Mutation to delete product
+   */
+  const { mutate: deleteProduct, isLoading: isDeleteLoading } = useMutation({
+    mutationFn: async () => {
+      const { data } = await axios.delete(
+        `/api/${params.storeId}/products/${targetId}`,
+      );
+      return data;
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        return toast.error(error.response?.data.message);
+      }
+
+      toast.error('Something went wrong');
+    },
+    onSuccess: (data) => {
+      startTransition(() => {
+        toast.success('Product deleted');
+        onClose();
+        router.refresh();
+      });
+    },
+  });
 
   return (
     <>
+      <AlertModal loading={isDeleteLoading} onConfirm={deleteProduct} />
+
       <div className="flex items-center justify-between">
         <Heading
           title={`Products (${data.length})`}
